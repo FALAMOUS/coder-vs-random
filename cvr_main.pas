6cfdba248050dc2c00636fdf30c87f4fc07a8120
Semyon Tsehnenko <watemus@gmail.com>
2017-01-26 18:10:18 +0300

    Init Lazarus project

---
+++ cvr_main.pas
@@ -0,0 +1,26 @@
+unit cvr_main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TFormMain = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  FormMain: TFormMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
